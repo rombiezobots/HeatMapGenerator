cc55ae91e87ab0613e201516cdae277587762770
Sam Van Hulle <sam@trailanimation.com>
2020-07-20 17:38:04 +0200

    improved user interface

--- panels.py
+++ panels.py
@@ -25,24 +25,47 @@ class VIEW3D_PT_heat_map_generator(bpy.types.Panel):
     bl_region_type = 'UI'
 
     def draw(self, context):
+        pass
 
-        settings = context.scene.heat_map_generator_settings
+
+class VIEW3D_PT_heat_map_measuring(bpy.types.Panel):
+
+    bl_label = 'Measure Vertex Distances'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_parent_id = 'VIEW3D_PT_heat_map_generator'
+
+    def draw(self, context):
         lay = self.layout
         lay.use_property_split = True
+        lay.use_property_decorate = False
         lay.prop(context.scene, 'use_preview_range', toggle=0)
         lay.prop(context.scene, 'frame_step')
         lay.separator()
         row = lay.row()
         row.scale_y = 1.5
         row.operator('heat_map_generator.run')
-        lay.separator()
+
+
+class VIEW3D_PT_heat_map_painting(bpy.types.Panel):
+
+    bl_label = 'Paint Weights'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_parent_id = 'VIEW3D_PT_heat_map_generator'
+
+    def draw(self, context):
+        settings = context.scene.heat_map_generator_settings
+        lay = self.layout
+        lay.use_property_split = True
+        lay.use_property_decorate = False
+        lay.enabled = functions.distance_dict_is_not_empty()
         lay.prop(settings, 'group_name')
         lay.separator()
         col = lay.column()
-        col.enabled = functions.distance_dict_is_not_empty()
         col.prop(settings, 'use_tresholds')
         col = lay.column(align=True)
-        col.enabled = settings.use_tresholds and functions.distance_dict_is_not_empty()
+        col.enabled = settings.use_tresholds
         col.prop(settings, 'weight_low_bound')
         col.prop(settings, 'weight_high_bound')
         lay.separator()
@@ -57,5 +80,7 @@ class VIEW3D_PT_heat_map_generator(bpy.types.Panel):
 
 
 register, unregister = bpy.utils.register_classes_factory([
-    VIEW3D_PT_heat_map_generator
+    VIEW3D_PT_heat_map_generator,
+    VIEW3D_PT_heat_map_measuring,
+    VIEW3D_PT_heat_map_painting
 ])
