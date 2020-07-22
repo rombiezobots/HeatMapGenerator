7443044c178b3e8ed98a590264528a85b5a3f693
Sam Van Hulle <sam@trailanimation.com>
2020-07-22 19:28:25 +0200

    switched to raycasting for self-occlusion (glitchy though)

--- functions.py
+++ functions.py
@@ -4,8 +4,8 @@
 
 
 import bpy
-import bmesh
 from bpy_extras.object_utils import world_to_camera_view
+from mathutils import Vector
 
 
 ###############################################################################
@@ -66,21 +66,38 @@ def calculate_distances():
             return scene.frame_preview_start, scene.frame_preview_end
         return scene.frame_start, scene.frame_end
 
-    def vertex_is_visible(vertex: bmesh.types.BMVert) -> bool:
+    def visible_face_from_vertex(vertex: bpy.types.MeshVertex) -> bpy.types.MeshPolygon:
         """Convert the vertex's coordinates into camera space, and check
-        whether its X and Y coordinates are within the frustum, and its
-        Z value between the clipping boundaries."""
+        whether its coordinates are within the frustum. Then cast a ray at it
+        to see whether it's occluded."""
         scene = bpy.context.scene
         cam = scene.camera
         ob = bpy.context.object
-        cs = scene.camera.data.clip_start
-        ce = scene.camera.data.clip_end
         cc = world_to_camera_view(scene,
                                   cam,
-                                  ob.matrix_world @ v.co)
-        return 0.0 < cc.x < 1.0 and 0.0 < cc.y < 1.0 and cs < cc.z < ce
+                                  ob.matrix_world @ vertex.co)
+        cs = cam.data.clip_start
+        ce = cam.data.clip_end
+        # If the vertex's screen coordinates are within camera view
+        if 0.0 < cc.x < 1.0 and 0.0 < cc.y < 1.0 and cs < cc.z < ce:
+            # Convert the screen coordinates to a 3D vector
+            frame = cam.data.view_frame(scene=scene)
+            top_left = frame[3]
+            pixel_vector = Vector((cc.x, cc.y, top_left[2]))
+            pixel_vector.rotate(cam.matrix_world.to_quaternion())
+            # Convert to target object space
+            origin = ob.matrix_world.inverted() @ (pixel_vector + cam.matrix_world.translation)
+            # The ray's destination is the original vertex, in object space
+            destination = ob.matrix_world.inverted() @ vertex.co
+            direction = (destination - origin).normalized()
+            # Cast a ray from those screen coordinates to the vertex
+            result, location, normal, index = ob.ray_cast(origin, direction)
+            if result and index > -1:
+                # Return the face the vertex belongs to
+                return ob.data.polygons[index]
+        return False
 
-    def distance_to_camera(vertex: bmesh.types.BMVert) -> float:
+    def distance_to_camera(vertex: bpy.types.MeshVertex) -> float:
         """Calculate the vertex's distance to the camera."""
         cam = bpy.context.scene.camera
         ob = bpy.context.object
@@ -93,10 +110,6 @@ def calculate_distances():
 
     # Clear the vertex distance dict
     vertex_distances.clear()
-    # Switch to camera view
-    for area in bpy.context.screen.areas:
-        if area.type == 'VIEW_3D':
-            area.spaces[0].region_3d.view_perspective = 'CAMERA'
     # Determine frame settings
     start_frame, end_frame = frame_range()
     # Store current mode
@@ -106,30 +119,29 @@ def calculate_distances():
     log(f'Using start frame {start_frame}, end frame {end_frame}, and step {scene.frame_step}')
     wm.progress_begin(0, 100)
 
-    # Create BMesh
-    bpy.ops.object.mode_set(mode='EDIT')
-    bmesh_data = bmesh.from_edit_mesh(ob.data)
     # Loop over frames
-    for i in range(start_frame, end_frame + 1, scene.frame_step):
-        # Set the current frame (using the method is important
-        # for Blender to be aware of this)
-        scene.frame_set(i)
-        # Get vertices from mesh
-        for v in bmesh_data.verts:
-            if vertex_is_visible(vertex=v):
-                distance = distance_to_camera(vertex=v)
-                if vertex_distances.get(v.index):
-                    if distance < vertex_distances.get(v.index):
-                        vertex_distances[v.index] = distance
-                else:
-                    vertex_distances[v.index] = distance
+    for frame in range(start_frame, end_frame + 1, scene.frame_step):
+        # Set the current frame (using the method is important for Blender to
+        # be aware of this)
+        scene.frame_set(frame)
+        # Get vertices from mesh. For each vertex, if it is visible, measure
+        # its distance to the camera. If it's lower than it ever has been, or
+        # hasn't been in view before, store it in the vertex_distances dict.
+        for vertex in ob.data.vertices:
+            visible_face = visible_face_from_vertex(vertex=vertex)
+            if visible_face:
+                for vertex_id in visible_face.vertices:
+                    vertex = ob.data.vertices[vertex_id]
+                    known_vertex = vertex_distances.get(vertex_id)
+                    distance = distance_to_camera(vertex=vertex)
+                    if not known_vertex or distance < known_vertex:
+                        vertex_distances[vertex_id] = distance
         # Update progress
-        log(f'Frame: {i}')
-        progress = int((i - start_frame) / (end_frame - start_frame + 1) * 100)
+        log(f'Frame: {frame}')
+        progress = int((frame - start_frame) /
+                       (end_frame - start_frame + 1) * 100)
         wm.progress_update(progress)
 
-    # Clean up the bmesh to lower memory impact
-    bmesh_data.free()
     # End progress indicator
     wm.progress_end()
     # Go back to previous mode
@@ -157,6 +169,7 @@ def paint_vertex_weights():
     # Normalize
     for index in vertex_distances:
         distance = vertex_distances[index]
+        # POTENTIAL DIVIDE BY ZERO ERROR
         normalised_distance = 1 - (distance - min_dist) / (max_dist - min_dist)
         vertex_group = bpy.context.object.vertex_groups.active
         vertex_group.add(index=[index],
